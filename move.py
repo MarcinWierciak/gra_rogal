0e88f39945ea47e748ac069d9da51f90c96d5820
Marcin Wierciak <marcin.f.w@gmail.com>
2017-08-31 13:30:19 +0200

    add movement module

---
+++ move.py
@@ -0,0 +1,50 @@
+import os
+import terrain
+import hero
+
+def getch():
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
+def go_player(board, x, y, sign, monsters, items, player_stats, chests): # porusza postacią gracza
+    direction = getch()
+    move_status = {"x_coord":0,"y_coord":0,"action":"", "found_object":""}
+    new_x = x
+    new_y = y
+    added_item = ""
+    if direction == "s":
+        new_x += 1
+    if direction == "w":
+        new_x -= 1
+    if direction == "a":
+        new_y -= 1
+    if direction == "d":
+        new_y += 1
+    new_xy = board[new_x][new_y]
+    if new_xy != "#":
+        terrain.insert_object(board, x, y, "·")
+        x = new_x
+        y = new_y
+    if new_xy in items:
+        terrain.insert_object(board, x, y, "·")
+        x = new_x
+        y = new_y
+        hero.add_item(board, new_xy, player_stats)
+    if new_xy in monsters:
+        print("GET READY FOR FIGHT!!")
+        move_status.update({"x_coord":x,"y_coord":y,"action":"fight","found_object":new_xy})
+        return move_status
+    if new_xy in chests:
+        print("You found chest. It's locked.")
+        move_status.update({"x_coord":x,"y_coord":y,"action":"chest", "found_object":new_xy})
+        return move_status
+    move_status.update({"x_coord":x,"y_coord":y})
+    return move_status
